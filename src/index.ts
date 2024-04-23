ec11e27bf5014113761d38e18501733b37556764
salahqasm <salahqasm@outlook.com>
2024-04-23 23:01:47 +0300

    init socket-io

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,33 @@
 import express, { Request, Response, NextFunction } from "express"
+import { Server } from "socket.io";
+import http from "http"
 import { PORT } from "./config";
-
+import cors from "cors"
 const app = express();
-
+const server = http.createServer(app);
+const io = new Server(server,{
+    cors:{
+        origin:"*"
+    }
+})
+app.use(cors());
 app.use(express.json());
 
-
-
-
-
-
+io.on('connection', (socket) => {
+    console.log("Connected")
+    io.emit("message", { data: "Connected" })
+    socket.on("message", (data) => {
+        console.log(data.message)
+        socket.emit("message", { event: "message", data:data.message })
+        if(data.message==="close"){
+            socket.disconnect();
+        }
+    })
+    socket.on("disconnect",(data)=>{
+        socket.emit("message",{event:"message",data:"good bye"})
+        console.log("disconnected")
+    })
+})
 
 
 
@@ -20,6 +38,6 @@ app.get('/health', (req: Request, res: Response) => {
     res.sendStatus(200)
 })
 
-app.listen(PORT,()=>{
+server.listen(PORT, () => {
     console.log(`********** Started listening on port: ${PORT} **********`)
 })
